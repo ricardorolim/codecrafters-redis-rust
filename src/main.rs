a252fe40f5b2ffcb86dd30c4918c5fc1c1348c2c
Ricardo Rolim <rickroll@amazon.com>
2024-04-20 18:24:50 +0100

    Respond to PING

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf = [0; 512];
+    let n = stream.read(&mut buf).expect("Failed to read from client");
+    println!("received {} bytes", n);
+    println!("data {:?}", &buf[..n]);
+
+    stream
+        .write_all(b"PONG\r\n")
+        .expect("Fail to write to client");
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
